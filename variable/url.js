c8aece480ed9a1d35b0d47cafd4fc3900aa49420
ZeesahanAhmad <54854866+ZeesahanAhmad@users.noreply.github.com>
2020-05-02 01:15:31 +0530

    Update url.js

--- variable/url.js
+++ variable/url.js
@@ -1,5 +1,5 @@
 module.exports={
-    // mongoURL:"mongodb+srv://zeeshan_ahmad:<mongo@123>@sports-9y92p.mongodb.net/test?retryWrites=true&w=majority",
+    // mongoURL:"mongodb+srv://zeeshan_ahmad:<zeeshan@123>@sports-9y92p.mongodb.net/test?retryWrites=true&w=majority",
     mongoURL:"mongodb+srv://zeeshan:zeeshan123@cluster0-sgtpb.mongodb.net/test?retryWrites=true&w=majority",
     secreteKey:"building-my-sport-web"
 }
